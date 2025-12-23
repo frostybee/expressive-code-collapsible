92679a8afa4ba2bc4774f0bd7a04f4e07a5ae44f
frostybee <rsleiman2@gmail.com>
2025-12-23 13:26:07 -0500

    Remove expressive-code-fullscreen and expressive-code-language-badge, update starlight-package-managers version, and adjust plugins in ec.config.mjs

--- docs/ec.config.mjs
+++ docs/ec.config.mjs
@@ -1,8 +1,7 @@
 import { pluginCollapsible } from 'expressive-code-collapsible'
-import { pluginLanguageBadge } from 'expressive-code-language-badge'
-import { pluginFullscreen } from 'expressive-code-fullscreen'
+
 
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
 export default {
-  plugins: [pluginCollapsible(), pluginLanguageBadge(), pluginFullscreen()],
+  plugins: [pluginCollapsible()],
 }
